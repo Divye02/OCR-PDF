973522e32d01d2b0950a4947f2884c296fc5b2f6
Divye Pratap Jain <dpjain@uw.edu>
2016-03-03 01:38:57 -0800

    json for first book ready

--- ocr.py
+++ ocr.py
@@ -1,4 +1,8 @@
 import httplib, urllib, base64
+import json
+import time
+
+length = 291 + 1;
 
 headers = {
     # Request headers
@@ -11,14 +15,22 @@ params = urllib.urlencode({
     'language': 'unk',
     'detectOrientation ': 'true',
 })
+data = [];
+conn = httplib.HTTPSConnection('api.projectoxford.ai')
+for i in range(0, length):
+    try:
+        if (i+1) % 20 == 0:
+            time.sleep(46)
+        conn.request("POST", "/vision/v1/ocr?%s" % params, "{'Url': 'http://divye02.github.io/OCR-PDF/test/page-" + str(i) + ".jpg'}", headers)
+        response = conn.getresponse()
+        data.append({'page' : i, 
+                    'content' : json.loads(response.read())
+                    })
+        #print(data)
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
+conn.close()
 
-try:
-    conn = httplib.HTTPSConnection('api.projectoxford.ai')
-    conn.request("POST", "/vision/v1/ocr?%s" % params, "{'Url': 'http://divye02.github.io/OCR-PDF/test/page-12.jpg'}", headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+with open('data.json', 'w') as outfile:
+    json.dump(data, outfile)
